e14bc1f2b0d5300c2734ed42703828c182420958
bllee29 <bllee0209@gmail.com>
2023-05-22 21:50:25 +0900

    question & answer posting

--- server/views/answer_views.py
+++ server/views/answer_views.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
+
 from server import db
 from server.models import Question, Answer
+from server.forms import AnswerForm
 
 # answer_vies.py 파일이 answer이름을 갖는 블루 프린트인것
 bp = Blueprint('answer', __name__, url_prefix='/answer')
@@ -11,9 +13,12 @@ bp = Blueprint('answer', __name__, url_prefix='/answer')
 # question_detail의 form의 method와 일치하게.
 @bp.route('/create/<int:question_id>', methods=('POST',))
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    content = request.form['content']
-    answer = Answer(content=content, create_date=datetime.now())
-    question.answer_set.append(answer)  # 질문의 답변
-    db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
+    if form.validate_on_submit():
+        content = request.form['content']
+        answer = Answer(content=content, create_date=datetime.now())
+        question.answer_set.append(answer)
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
+    return render_template('question/question_detail.html', question=question, form=form)
--- server/views/question_views.py
+++ server/views/question_views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
-
+from datetime import datetime
+from flask import Blueprint, render_template, url_for, request
+from werkzeug.utils import redirect
+from .. import db
 from server.models import Question
+from ..forms import QuestionForm, AnswerForm
 
 # question_views.py 파일이 question 이름을 갖는 블루 프린트인것
 bp = Blueprint('question', __name__, url_prefix='/question')
@@ -14,5 +17,20 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    form = QuestionForm() # form객체 생성후 return에 전달.
+    # POST면 데이터 저장하고 메인페이지로
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data,
+                            create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    # GET이면 질문요청 페이지로 간다.
+    return render_template('question/question_form.html', form=form)
