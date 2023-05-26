02ba16c0f089162c9c54d7daf653997ec2182d67
bllee29 <bllee0209@gmail.com>
2023-05-26 21:41:08 +0900

    add user table

--- server/models.py
+++ server/models.py
@@ -17,3 +17,10 @@ class Answer(db.Model):
     # answer_set의 이름으로 question에서 해당 question에 1대다관계로 매핑된 answer들에 접근 할 수 있다.
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
