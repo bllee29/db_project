fa208ef5c808eae055aa3114be2a19e5a4bdc452
bllee29 <bllee0209@gmail.com>
2023-05-29 20:59:01 +0900

    display username

--- server/models.py
+++ server/models.py
@@ -3,20 +3,24 @@ from server import db
 
 # question table
 class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # 자동으로 부여되는 글번호
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set'))
 
 
 # answer table
 class Answer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # 자동으로 부여되는 글번호
     question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
     question = db.relationship('Question', backref=db.backref('answer_set'))
     # answer_set의 이름으로 question에서 해당 question에 1대다관계로 매핑된 answer들에 접근 할 수 있다.
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('answer_set'))
 
 
 class User(db.Model):
